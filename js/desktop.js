dc684a85dca31c06928a3effc70cd46b659f4efe
leoyulee <leoyulee.585@gmail.com>
2021-05-22 01:42:22 -0500

    Added comments

--- js/desktop.js
+++ js/desktop.js
@@ -1,11 +1,11 @@
 $(function() {
-    $('.page-link').click(function() {
-        var anchor = $(this).attr('dest');
-        $('.link-wrap').removeClass('visible');
-        $('nav span').removeClass('active');
+    $('.page-link').click(function() {                                          //Select anything with the class of .... page-link, and listen to when it is clicked.
+        var anchor = $(this).attr('dest');                                      //Grab the intended destination of the custom attribute "dest"
+        //$('.link-wrap').removeClass('visible'); 
+        $('nav span').removeClass('active');                                    //Disable this button
         $('nav')
-        .find('[dest="' + anchor + '"]')
-        .addClass('active');
-        $('html, body').animate({scrollTop: $('#' + anchor).offset().top},400);
+        .find('[dest="' + anchor + '"]')                                        //Find the object with an id of var anchor
+        .addClass('active');                                                    //Enable this button
+        $('html, body').animate({scrollTop: $('#' + anchor).offset().top},400); //Go to the destination
     });
 });
